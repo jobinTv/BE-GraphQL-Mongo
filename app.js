1942a05b1a0bd743e22394bb387328ce21715767
Jobin T v <jobintv@focaloid.com>
2022-06-24 15:37:35 +0530

    feat: Mongo connection setup completed

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+require('dotenv').config({
+  path: `${__dirname}/.env.local`
+})
 const express = require('express');
 const app = express();
 const PORT = 8008;
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schemas');
+const mongo = require('./config/mongo');
 
+app.use(express.json());
+
+(async () => {
+  try{
+    await mongo.connect();
+  }
+  catch(e){
+    console.log(e, 'error');
+  }
+  console.log('Mongodb connection completed');
+})()
 app.use('/graphql',graphqlHTTP({
     schema,
     graphiql: true
---
+++ config/mongo.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+
+const uri = process.env.MONGO_URL;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+
+const state = {client: null};
+
+
+exports.connect = async () => {
+    try{
+        console.log('hiii')
+        state.client = await client.connect();
+    }
+    catch (e){
+        console.log(e, 'error');
+    }
+}
+
+exports.close =  async () => {
+    await state.client.close();
+    return true;
+}
+
+exports.get = () => state.client.db();
